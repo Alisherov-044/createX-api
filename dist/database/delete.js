e8e56a3982712975f174174a20373aa040bb0fee
Alisherov-044 <alisherov044@gmail.com>
2024-03-18 15:08:52 +0500

    feat: done!

--- dist/database/delete.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteData = void 0;
-const _1 = require(".");
-function deleteData(tableName, deleteBy, callbackFn) {
-    _1.db.run(`DELETE FROM ${tableName} WHERE ${Object.keys(deleteBy)[0]} = ?`, [Object.values(deleteBy)[0]], callbackFn);
-}
-exports.deleteData = deleteData;
--- dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = exports.PORT = void 0;
-var contants_1 = require("./contants");
-Object.defineProperty(exports, "PORT", { enumerable: true, get: function () { return contants_1.PORT; } });
-var routes_1 = require("./routes");
-Object.defineProperty(exports, "routes", { enumerable: true, get: function () { return routes_1.routes; } });
